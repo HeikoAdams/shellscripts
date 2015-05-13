0a0005c3fdc662683bd39df07dc1954c89d34697
Heiko Adams <heiko.adams@gmail.com>
2015-05-13 22:32:17 +0200

    Use full path for executing binaries

--- wetterdienst.sh
+++ wetterdienst.sh
@@ -2,7 +2,7 @@
 notify_header="Wetterwarnungen für Coburg"
 popup_icon=~/Bilder/Wetterwarnung.png
 landkreis=COX
-image_viewer=display # must be able to handle URLs
+image_viewer=/usr/bin/display # must be able to handle URLs
 warning_url='http://www.dwd.de/dyn/app/ws/html/reports/'${landkreis}'_warning_de.html'
 timeline_url="http://www.dwd.de/dyn/app/ws/maps/${landkreis}_timeline.png"
 automode=false
@@ -15,20 +15,20 @@ if [ -n "$1" ]; then
         size=${#i}
         if [ "$size" -eq 3 ]; then
             landkreis=$1
-            notify_header="Wetterwarnungen für "$landkreis
+            /usr/bin/notify_header="Wetterwarnungen für "$landkreis
         else
-            notify-send --icon=$popup_icon """ungültiger Landkreis""" "ungültiger Landkreis: $1"
+            /usr/bin/notify-send --icon=$popup_icon """ungültiger Landkreis""" "ungültiger Landkreis: $1"
             exit
         fi
     fi
 fi
 
-textstring=$(wget $warning_url -q -O -  | grep -i -e "warnung vor" -e "vorabinformation" | sed s/\<\\/p\>//g ) 
+textstring=$(/usr/bin/wget $warning_url -q -O -  | grep -i -e "warnung vor" -e "vorabinformation" | sed s/\<\\/p\>//g ) 
 
 if [ "$textstring" = ""  ]; then 
-    notify-send --icon=$popup_icon """$notify_header""" "keine Warnungen vorhanden"
+    /usr/bin/notify-send --icon=$popup_icon """$notify_header""" "keine Warnungen vorhanden"
 else 
-    notify-send --icon=$popup_icon """$notify_header""" """$textstring"""
+    /usr/bin/notify-send --icon=$popup_icon """$notify_header""" """$textstring"""
     if [ "$automode" == false ]; then
         $image_viewer $timeline_url &
         # xdg-open $warning_url &
