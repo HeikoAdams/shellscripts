e3bf8a0d35a410d4fbba151ec0bf3b5cd957e86e
Heiko Adams <heiko.adams@gmail.com>
2015-07-26 22:10:10 +0100

    Add gpl header

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
+# build.sh - a script to build Fedora packages localy using mock and upload them to COPR
+#
+#  Copyright (C) 2015 Heiko Adams heiko.adams@gmail.com
+#
+#  This source is free software; you can redistribute it and/or modify it under
+#  the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 2 of the License, or (at your option)
+#  any later version.
+#
+#  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#  details.
+#
+#  A copy of the GNU General Public License is available on the World Wide Web
+#  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+#  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+#  MA 02111-1307, USA.
+#
+
 function prepareBuild {
     WDIR=$(pwd)
     
