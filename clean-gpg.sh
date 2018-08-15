df58fa0a388b467fa1db7bac411441aa313edcdc
Heiko Adams <h.adams@heiko-adams.de>
2018-08-15 16:24:52 +0200

    Bugfixes

--- clean-gpg.sh
+++ clean-gpg.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Clean up the GPG Keyring.  Keep it tidy.
-# blog.lavall.ee
 
-echo -n "Expired Keys: "
-for expiredKey in $(gpg2 --list-keys --fixed-list-mode --with-colons  | grep "^pub" | grep "^pub:[r|e]:" | cut -f5 -d":" | fmt -w 999); do
+filter="^pub:[r|e]:"
+
+echo -n "Expired and revoked Keys: "
+for expiredKey in $(gpg2 --list-keys --fixed-list-mode --with-colons  | grep "^pub" | grep "$filter" | cut -f5 -d":" | fmt -w 999); do
     echo -n "$expiredKey"
     gpg2 --batch --yes --quiet --delete-keys $expiredKey >/dev/null 2>&1
     if [ $? -eq 0 ]; then
@@ -15,9 +16,9 @@ done
 echo done.
 echo
 echo -n "Update Keys: "
-for keyid in $(gpg2 --list-keys --fixed-list-mode --with-colons  | grep "^pub" | grep -v "^pub:[r|e]:" | cut -f5 -d":" | fmt -w 999); do
+for keyid in $(gpg2 --list-keys --fixed-list-mode --with-colons  | grep "^pub" | grep -v "$filter" | cut -f5 -d":" | fmt -w 999); do
     echo -n "$keyid"
-    gpg2 --batch --quiet --edit-key "$keyid" check clean cross-certify save quit > /dev/null 2>&1
+    gpg2 --batch --yes --quiet --edit-key "$keyid" check clean cross-certify save quit > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo -n "(OK), "
     else
