340af7605b7b04168dd9ab2c47224e9729c88d5e
Heiko Adams <h.adams@heiko-adams.de>
2021-03-14 15:00:18 +0100

    Prüfung, ob FF oder TB noch ausgefürt werden, arbeitet jetzt zuverlässiger

--- compress.sh
+++ compress.sh
@@ -11,10 +11,22 @@ TBDIR="${HOME}/.thunderbird"
 echo "Prüfe, ob Firefox oder Thunderbird noch ausgeführt werden"
 FFRUNNING=$(find "$FFDIR" -name lock)
 TBRUNNING=$(find "$TBDIR" -name lock)
+FFCOUNT=$(ps -ef | grep firefox | wc -l)
+TBCOUNT=$(ps -ef | grep thunderbird | wc -l)
 
 # Funktion, um zu prüfen, ob das Programm noch ausgeführt wird
 function check_running {
   if [ -n "$1" ]
+  then
+    if [ $4 -eq 1 ]
+    then
+      rm -f "$1"
+    else
+      echo -e "\n${2} wird noch ausgeführt!\nBitte beenden Sie ${2}, bevor Sie die"
+      echo -e "Datenbanken komprimieren!\n";
+      exit $3;
+   fi
+  elif [ $4 -gt 1 ]
   then
     echo -e "\n${2} wird noch ausgeführt!\nBitte beenden Sie ${2}, bevor Sie die"
     echo -e "Datenbanken komprimieren!\n";
@@ -36,8 +48,8 @@ function shrink_dbs {
 }
 
 # prüfen, ob FF und TB noch laufen
-check_running "$FFRUNNING" "Firefox" -110
-check_running "$TBRUNNING" "Thunderbird" -210
+check_running "$FFRUNNING" "Firefox" -110 $FFCOUNT
+check_running "$TBRUNNING" "Thunderbird" -210 $TBCOUNT
 
 # Datenbanken komprimieren
 shrink_dbs "Firefox" "$FFDIR"
