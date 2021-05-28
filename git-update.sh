c005a9405111fea91d5a85d67cbaf3d127ec6892
Heiko Adams <h.adams@heiko-adams.de>
2021-05-28 23:08:07 +0200

    Script kommentiert

--- git-update.sh
+++ git-update.sh
@@ -1,18 +1,25 @@
 #! /bin/bash
 
+# Standardwerte für Optionen setzen
 PRJDIR="$HOME/Projekte"
 CLEAN=true
 
+# Parameter verarbeiten
 while [ $# -gt 0 ]
 do                   
     if [ "$1" == "--noclean" ]; then
         CLEAN=false
     elif [ "$1" == "--dir" ] && [ -n "$2" ] && [ -d "$2" ]; then
         PRJDIR="$2"
+    else
+        echo "unbekannter Parameter $1"
+        exit -1
     fi
     shift
 done
 
+# Im Projektverzeichnis nach git Repositories suchen
+# und diese aktualisieren
 cd $PRJDIR
 echo "suche zu aktualisierende git Repositories"
 for REPO in $(find -name .git -type d | sort); do
@@ -24,6 +31,9 @@ for REPO in $(find -name .git -type d | sort); do
     cd "$REPOPATH"
     echo "Aktualisiere $REPONAME"
     UPSTREAM=$(git remote -v | grep ups)
+
+    # Wenn es einen Upstream-Branch gibt, Änderungen mergen und pushen,
+    # ansonten nur Änderungen herunterladen
     if [ -n "$UPSTREAM" ]; then
         echo "Synchronisiere mit Upstream"
         git fetch upstream
